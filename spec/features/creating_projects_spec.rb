68112a98a3aaa50cb828ffc88fa181fd46ce4ec3
Dan Kleiman <info@dankleiman.com>
2014-07-29 16:44:46 -0400

    Added title to show page

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -2,14 +2,22 @@ require 'spec_helper'
 
 feature 'Creating Projects' do
   scenario 'can create a project' do
+    project_name = "TextMate 2"
     visit '/'
 
     click_link 'New Project'
 
-    fill_in 'Name', with: 'TextMate 2'
+    fill_in 'Name', with: project_name
     fill_in 'Description', with: 'A text editor for OS X'
     click_button 'Create Project'
 
     expect(page).to have_content('Project has been created.')
+
+    project = Project.where(name: project_name).first
+
+    expect(page.current_url).to eql(project_url(project))
+
+    title = "#{project_name} - Projects - Ticket Tracker"
+    expect(page).to have_title(title)
   end
 end
